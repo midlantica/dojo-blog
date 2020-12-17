a3a762ccb6512365276d4a69143da7ab7fb3ab31
Drew Harper <druharper@gmail.com>
2020-12-16 19:20:54 -0600

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <h1>Home</h1>
+    <p>My name is {{ name }} and I am {{ age }} years old</p>
+    <button @click="handleClick">Click me Drewby Doo</button>
+    <button @click="age++">Make me older</button>
+    <br><br>
+    <input type="text" v-model='name'>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  import { ref } from 'vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    setup() {
+
+      const name = ref('Drooby Doo')
+      const age = ref(32)
+
+      const handleClick = () => {
+        name.value = 'Freddy'
+        age.value = 35
+        // console.log(p, p.value)
+        // p.value.classList.toggle('test')
+      }
+
+      return {
+        name: name,
+        age: age,
+        handleClick
+      }
+    }
   }
-}
 </script>
+
+<style>
+  .test {
+    color: red;
+  }
+</style>
