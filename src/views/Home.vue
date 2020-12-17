ac016f6a5d8e1842dc9105fcef96bbd17453b351
Drew Harper <druharper@gmail.com>
2020-12-16 19:56:18 -0600

    computed properties

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,76 +1,33 @@
 <template>
   <main>
+
     <h1>Home</h1>
-    <p>My name is {{ name }} and I am {{ age }} years old</p>
-    <button @click="handleClick">Click me Drewby Doo</button>
-    <br><br>
-    <input type="text" v-model='name'>
-    <br><br>
-    <button @click="age++">Make me older</button>
-    <br><br>
-    <h4>Refs</h4>
-    <p>{{ ninjaOne.name }} – {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">updateNinjaOne</button>
-    <br><br>
-    <h4>Reactive ie two way</h4>
-    <p>{{ ninjaTwo.name }} – {{ ninjaTwo.age }}</p>
-    <label>Update Ninja Two</label>
-    <input @click="updateNinjaTwo">
-    <button @click="updateNinjaTwo">updateNinjaTwo</button>
+    <input type="text" v-model="search">
+    <p>Search term:</p>
+    <p>{{search || "––"}}&nbsp;</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+
   </main>
 </template>
 
 <script>
-  import { ref, reactive } from 'vue'
+  import { computed, ref } from 'vue'
 
   export default {
     name: 'Home',
     setup() {
+      const search = ref('')
+      const names = ref(['Fred', 'Jim', 'Valentina', 'Ava' ])
 
-      const name = ref('Drooby Doo')
-      const age = ref(32)
-
-      const ninjaOne = ref({
-        name: 'Jason',
-        age: 51
+      const matchingNames = computed(() => {
+        return names.value.filter((name) => name.includes(search.value))
       })
 
-      const ninjaTwo = reactive({
-        name: 'Andrew',
-        age: 53
-      })
-
-      const updateNinjaOne = () => {
-        ninjaOne.value.age = 29
-      }
-
-      const updateNinjaTwo = () => {
-        ninjaTwo.age = 29
-      }
-
-
-      const handleClick = () => {
-        name.value = 'Freddy'
-        age.value = 35
-        // console.log(p, p.value)
-        // p.value.classList.toggle('test')
-      }
-
-      return {
-        name: name,
-        age: age,
-        handleClick,
-        ninjaOne,
-        updateNinjaOne,
-        ninjaTwo,
-        updateNinjaTwo
-      }
+      return { names, search, matchingNames }
     }
   }
 </script>
 
 <style>
-  .test {
-    color: red;
-  }
+
 </style>
