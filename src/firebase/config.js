d99bc3ea243aa0236e4d237e365cf3c4f44de796
Drew Harper <druharper@gmail.com>
2021-01-06 15:46:17 -0600

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import fireebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAW0XYKSriNE-MOQPATa-2OcJDUCtFSMN8",
+  authDomain: "ninja-91-31d7e.firebaseapp.com",
+  projectId: "ninja-91-31d7e",
+  storageBucket: "ninja-91-31d7e.appspot.com",
+  messagingSenderId: "95861582332",
+  appId: "1:95861582332:web:f82e89d91bc7a8c311aa79"
+}
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// inite firestore service
+const projectFirestore = firebase.firestore()
+
+export { projectFirestore }
